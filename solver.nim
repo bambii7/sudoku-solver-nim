7a29de22fae9286fa920b1f393ab8bbb42a9c443
Alexis <alexis@cvofhope.com>
2018-09-22 18:05:57 +1000

    add string replace proc

--- solver.nim
+++ solver.nim
@@ -1,8 +1,13 @@
 import os
 
+proc strReplaceChar(str: string, index: int, character: char): string =
+  str[0..index - 1] & character & str[index + 1..str.high]
+
 proc solve(puzzle: string):string =
   let i:int = puzzle.find('0')
-  return puzzle[i..i]
+  
+  let newPuzzle = strReplaceChar(puzzle, i, 'a')
+  return newPuzzle
 
 if paramCount() == 1:
   let puzzle:string = paramStr(1)
