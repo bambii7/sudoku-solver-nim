4e72cdc918ffc9a65c1020de30fe4a374a4b26c3
Alexis <alexis@cvofhope.com>
2018-09-24 15:23:06 +1000

    add solver consts

--- solver.nim
+++ solver.nim
@@ -1,5 +1,8 @@
 import os
 
+const sudoku_range: int8 = 81
+const sudoku_numbers: string = "123456789"
+
 proc strReplaceChar(str: string, index: int, character: char): string =
   str[0..index - 1] & character & str[index + 1..str.high]
 
@@ -8,8 +11,10 @@ proc solve(puzzle: string): string =
   if i == -1:
     return puzzle
 
-  let newPuzzle: string = strReplaceChar(puzzle, i, 'a')
-  return newPuzzle
+  var exclude: set[char]
+
+  let new_puzzle: string = strReplaceChar(puzzle, i, 'a')
+  return new_puzzle
 
 if paramCount() == 1:
   let puzzle:string = paramStr(1)
