b3eadb5c11950304363fc6038f05c1bc710f98e9
Alexis <alexis@cvofhope.com>
2018-12-24 11:43:04 +1100

    update int8 to int

--- sudokusolver/lib/math.nim
+++ sudokusolver/lib/math.nim
@@ -1,12 +1,12 @@
 
-proc sameColum*(currentSearch: int8, gameTile: int8): bool =
+proc sameColum*(currentSearch: int, gameTile: int): bool =
   currentSearch - gameTile mod 9 == 0
 
-proc sameRow*(currentSearch: int8, gameTile: int8): bool =
+proc sameRow*(currentSearch: int, gameTile: int): bool =
   currentSearch div 9 == gameTile div 9
 
-proc sameBlock*(currentSearch: int8, gameTile: int8): bool =
+proc sameBlock*(currentSearch: int, gameTile: int): bool =
   currentSearch div 27 == gameTile div 27 and currentSearch mod 9 div 3 == gameTile mod 9 div 3
 
-proc excludeable*(currentSearch: int8, gameTile: int8): bool =
-  sameColum(currentSearch, gameTile)
+proc excludeable*(currentSearch: int, gameTile: int): bool =
+  sameColum(currentSearch, gameTile) or sameRow(currentSearch, gameTile) or sameBlock(currentSearch, gameTile)
