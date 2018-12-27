7798db612bec48257f784fe0743f7506b7062fea
Alexis <alexis@cvofhope.com>
2018-12-27 11:09:41 +1100

    update map to functional style

--- sudokusolver/sudokusolver.nim
+++ sudokusolver/sudokusolver.nim
@@ -6,7 +6,7 @@ import sequtils
 when isMainModule:
   if paramCount() == 1:
     let puzzle:string = paramStr(1)
-    let puzzles:seq[int] = map(puzzle, charToInt)
+    let puzzles:seq[int] = puzzle.map(charToInt)
 
     echo "solving: " & toString(puzzles)
     let solution = backtrace(puzzles)
