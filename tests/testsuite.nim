c70aeeb1b2bd4d2b3ab828461233bbea120817dc
Alexis <alexis@cvofhope.com>
2018-12-28 11:12:45 +1100

    fix tests

--- tests/testsuite.nim
+++ tests/testsuite.nim
@@ -4,20 +4,12 @@ import solver/backtrace
 import sequtils
 
 suite "description for this stuff":
-  echo "suite setup: run once before the tests"
-  
-  setup:
-    echo "run before each test"
-  
-  teardown:
-    echo "run after each test"
-  
   test "sameColum returns true given 2, 11":
     let samesies = sameColum(2, 11)
     check(samesies == true)
 
-  test "sameColum returns false given 1, 81":
-    let samesies = sameColum(1, 81)
+  test "sameColum returns false given 1, 74":
+    let samesies = sameColum(1, 74)
     check(samesies == false)
 
   test "sameRow returns true given 2, 3":
@@ -29,18 +21,18 @@ suite "description for this stuff":
     check(samesies == false)
 
   test "sameBlock returns true given 1, 12":
-    let empty = sameRow(1, 12)
+    let empty = sameBlock(1, 12)
     check(empty == false)
 
   test "sameBlock returns true given 1, 18":
-    let empty = sameRow(1, 12)
+    let empty = sameBlock(1, 18)
     check(empty == true)
 
-  test "solve returns nil if invalid input":
+  test "backtrace returns nil if invalid input":
     let result = backtrace(@[1, 2, 3])
     check(result == nil)
 
-  test "solve returns nil if invalid input":
+  test "backtrace returns a valid solution":
     let puzzle = "010020300004005060070000008006900070000100002030048000500006040000800106008000000".map(charToInt)
     let result = backtrace(puzzle)
     let expected = "815624397394785162276139458126935478458167392739248156512376849347829156698145237".map(charToInt)
